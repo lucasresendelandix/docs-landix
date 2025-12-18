2add86f15ee78eac7ba95326a271f189f78dbae6
Lucas Resende <lucasresende@landix.com.br>
2025-12-18 14:43:39 -0300

    Deploy website - based on 3f77e5c4709ca4d6eb0d575983f851ec3c200058

---
+++ assets/js/fec652a9.12fe9984.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkmy_website=globalThis.webpackChunkmy_website||[]).push([[91],{6542(e){e.exports=JSON.parse('{"metadata":{"permalink":"/docs-landix/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
--- docs/intro.md
+++
@@ -1,7 +0,0 @@
----
-sidebar_position: 1
----
-
-# Introdução
-
-Esta página é focada em ter uma base de conhecimento dos produtos oferecidos pela Landix Sistemas.
